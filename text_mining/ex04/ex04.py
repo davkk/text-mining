327f2b0b50794a4f977fb8cf195b487759e861da
Dawid Karpiński <dawid.karp@int.pl>
2024-11-28 23:27:28 +0100

    feat: finish ex04

--- text_mining/ex04/ex04.py
+++ text_mining/ex04/ex04.py
@@ -1,19 +1,34 @@
+# %%
+import seaborn as sns
 import tmtoolkit as tm
+from matplotlib import gridspec
 from matplotlib import pyplot as plt
 from nltk.corpus import opinion_lexicon
+from wordcloud import WordCloud
 
 plt.rcParams["xtick.direction"] = "in"
 plt.rcParams["ytick.direction"] = "in"
 plt.rcParams["axes.grid"] = True
 plt.rc("figure", titlesize=22)
 
-corp = {
-    "Letters From a Stoic": open("./letters-from-stoic.txt").read(),
-}
-corp = tm.corpus.Corpus(corp, language="en")
+fig = plt.figure(figsize=(16, 16))
+gs = gridspec.GridSpec(2, 2, figure=fig)
+
+ax_emotion = fig.add_subplot(gs[:, 0])
+ax_cloud = fig.add_subplot(gs[0, 1])
+ax_pos = fig.add_subplot(gs[1, 1])
+
+
+# %%
+corp = tm.corpus.Corpus(
+    {"Crime and Punishment": open("./crime-and-punishment.txt").read()},
+    language="en",
+)
 tm.corpus.filter_clean_tokens(corp)
 tm.corpus.to_lowercase(corp)
 
+
+# %%
 tokens = tm.corpus.tokens_table(corp)
 top_3_nouns = (
     tokens[tokens["pos"] == "NOUN"]
@@ -22,19 +37,77 @@ top_3_nouns = (
     .head(3)["token"]
     .tolist()
 )
-print(f"{top_3_nouns=}")
 
+
+# %%
 corp_bi = tm.corpus.corpus_ngramify(corp, n=2, inplace=False)
 tokens_bi = tm.corpus.tokens_table(corp_bi)
-tokens_bi = tokens_bi[tokens_bi["token"].apply(lambda x: x.split()[1] in top_3_nouns)]
-tokens_bi = tokens_bi.drop_duplicates(subset=["token"])
 
+tokens_bi[["token", "noun"]] = tokens_bi["token"].str.split(" ", n=1, expand=True)
+
+tokens_bi = tokens_bi[tokens_bi["noun"].isin(top_3_nouns)]
+
+tokens_bi["pos"] = tokens_bi["pos"].apply(lambda x: x.split()[0])
+tokens_bi["tag"] = tokens_bi["tag"].apply(lambda x: x.split()[0])
+
+df = tokens_bi[["token", "noun", "pos"]]
+df_uniq = df.drop_duplicates(subset=["token"])
+
+
+# %%
+pos_hist = df_uniq["pos"].value_counts().reset_index()
+sns.barplot(
+    x="pos",
+    y="count",
+    data=pos_hist,
+    palette="viridis",
+    ax=ax_pos,
+)
+ax_pos.set_title("POS histogram")
+
+
+# %%
+wordcloud = WordCloud(background_color="white").generate(" ".join(df_uniq["token"]))
+ax_cloud.imshow(wordcloud, interpolation="bilinear")
+ax_cloud.axis("off")
+ax_cloud.set_title("Word Cloud", fontsize=16)
+
+
+# %%
 pos = set(opinion_lexicon.positive())
-tokens_bi["pos"] = tokens_bi["token"].apply(lambda x: x.split()[0] in pos)
+df["positive"] = df["token"].apply(lambda x: x.split()[0] in pos)
 
 neg = set(opinion_lexicon.negative())
-tokens_bi["neg"] = tokens_bi["token"].apply(lambda x: x.split()[0] in neg)
+df["negative"] = df["token"].apply(lambda x: x.split()[0] in neg)
 
-df = tokens_bi[tokens_bi["pos"] | tokens_bi["neg"]]
+df = df[df["positive"] | df["negative"]]
 
-print(df)
+
+# %%
+emotions = (
+    df.groupby(["token", "positive", "negative"]).size().reset_index(name="count")
+)
+emotions["score"] = (
+    emotions["count"] * emotions["positive"] - emotions["count"] * emotions["negative"]
+)
+emotions = emotions.sort_values(by="score", ascending=False)
+
+sns.barplot(
+    x="score",
+    y="token",
+    data=emotions,
+    orient="h",
+    palette=["green" if score > 0 else "red" for score in emotions["score"]],
+    ax=ax_emotion,
+)
+ax_emotion.grid(False, axis="x")
+ax_emotion.grid(True, axis="y")
+ax_emotion.set_title(f"Emotion score for words to left of top 3 nouns {top_3_nouns}")
+ax_emotion.set_xlabel("emotion score (positive=green, negative=red)")
+
+
+# %%
+fig.suptitle("Results for Crime and Punishment by Fyodor Dostoyevsky")
+fig.tight_layout()
+
+fig.savefig("results.pdf")
